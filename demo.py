8ddd2032fae7be172ab3ef4c3985ac265ad65eb1
David <39265971+somervda@users.noreply.github.com>
2024-11-12 13:48:11 -0500

    Added pcf8575 device

--- demo.py
+++ demo.py
@@ -4,6 +4,8 @@ from ssd1306 import SSD1306_I2C
 # See https://github.com/peterhinch/micropython-font-to-py
 import freesansnum35
 from machine import Pin, I2C
+import pcf8575
+import time
 
 i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 
@@ -13,11 +15,19 @@ for device in i2c.scan():
 OLED_WIDTH = 128
 OLED_HEIGHT = 64
 oled = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, addr=0x3C)
+pcf = pcf8575.PCF8575(i2c, 0x20)
 
-oled.line(0, 15, oled.width - 1, 15, 1)
-wri = Writer(oled, freesansnum35, verbose=False)
-Writer.set_textpos(oled, 0, 0)
-oled.text("Volts",  40, 0)
-Writer.set_textpos(oled, 25, 0)
-wri.printstring("12.74")
+for n in range(50):
+    button=pcf.pin(0)
+    oled.fill(0)
+    oled.line(0, 15, oled.width - 1, 15, 1)
+    wri = Writer(oled, freesansnum35, verbose=False)
+    Writer.set_textpos(oled, 0, 0)
+    oled.text("n: " + str(n),  40, 0)
+    Writer.set_textpos(oled, 25, 0)
+    wri.printstring(str(button))
+    oled.show()
+    time.sleep(.2)
+
+oled.fill(0)
 oled.show()
